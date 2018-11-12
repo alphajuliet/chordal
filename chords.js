8721d82297eb92bd37a8b6dc9a8996c53b3ad361
Glitch (hello-express) <none>
2018-11-12 23:36:30 +0000

    👓🏊🏼 Checkpoint ./chords.js:51581/2402

--- chords.js
+++ chords.js
@@ -27,28 +27,26 @@ const notes = [
 // See https://en.wikipedia.org/wiki/Chord_names_and_symbols_(popular_music)
 // @@TODO Add alternative chord names
 
-const chords = {
-    "maj":     [0, 4, 7],         // major (C-E-G)
-    "min":     [0, 3, 7],         // minor (C-E♭-G)
-    "7":       [0, 4, 7, 10],     // 7th (C-E-G-B♭)
-    "min7":    [0, 3, 7, 10],     // minor 7th (C-E♭-G-B♭)
-    "maj7":    [0, 4, 7, 11],     // major 7th (C-E-G-B)
-    "minmaj7": [0, 3, 7, 11],     // minor/major 7th (C-E♭-G-B)
-    "dim":     [0, 3, 6],         // diminished (C-E♭-G♭)
-    "aug":     [0, 4, 8],         // augmented (C-E-G♯)
-    "dim7":    [0, 3, 6, 9],      // diminished 7th (C-E♭-G♭-B𝄫)
-    "aug7":    [0, 4, 8, 10],     // augmented 7th (C-E-G♯-B♭)
-    "hdim7":   [0, 3, 6, 10],     // half-diminished 7th (C-E♭-G♭-B♭)
-    "maj6":    [0, 4, 7, 9],      // major 6th (C-E-G-A)
-    "sus2":    [0, 2, 7],         // suspended 2nd (C-D-G)
-    "sus4":    [0, 5, 7],         // suspended 4th (C-F-G)
-    "maj9":    [0, 4, 7, 11, 14], // major 9th (C-E-G-B-D)
-    "min9":    [0, 3, 7, 10, 14]  // minor 9th (C-E♭-G-B♭-D)
-}
+const chords = [
+	{ name: "maj",     notes: [0, 4, 7], 				 description: "major (C-E-G)" },
+	{ name: "min",     notes: [0, 3, 7],         description: "minor (C-E♭-G)" },
+	{ name: "7",       notes: [0, 4, 7, 10],     description: "7th (C-E-G-B♭)" },
+	{ name: "min7",    notes: [0, 3, 7, 10],     description: "minor 7th (C-E♭-G-B♭)" },
+	{ name: "maj7",    notes: [0, 4, 7, 11],     description: "major 7th (C-E-G-B)" },
+	{ name: "minmaj7", notes: [0, 3, 7, 11],     description: "minor/major 7th (C-E♭-G-B)" },
+	{ name: "dim",     notes: [0, 3, 6],         description: "diminished (C-E♭-G♭)" },
+	{ name: "aug",     notes: [0, 4, 8],         description: "augmented (C-E-G♯)" },
+	{ name: "dim7",    notes: [0, 3, 6, 9],      description: "diminished 7th (C-E♭-G♭-B𝄫)" },
+	{ name: "aug7",    notes: [0, 4, 8, 10],     description: "augmented 7th (C-E-G♯-B♭)" },
+	{ name: "hdim7",   notes: [0, 3, 6, 10],     description: "half-diminished 7th (C-E♭-G♭-B♭)" },
+	{ name: "maj6",    notes: [0, 4, 7, 9],      description: "major 6th (C-E-G-A)" },
+	{ name: "sus2",    notes: [0, 2, 7],         description: "suspended 2nd (C-D-G)" },
+	{ name: "sus4",    notes: [0, 5, 7],         description: "suspended 4th (C-F-G)" },
+	{ name: "maj9",    notes: [0, 4, 7, 11, 14], description: "major 9th (C-E-G-B-D)" },
+	{ name: "min9",    notes: [0, 3, 7, 10, 14], description: "minor 9th (C-E♭-G-B♭-D)" },
+]
 
-const chord_names = () => {
-  return {'chords': R.keys(chords)}
-}
+const chord_names = () => chords
 
 
 // ---------------------------------
